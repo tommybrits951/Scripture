f374b4403dbd2cfd4d7d49e7faa2638dc76cf870
tommybrits951 <tommybrits74@gmail.com>
2023-05-28 07:30:14 -0700

    finished with app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,30 +3,24 @@ import { MainContext } from '../App'
 function Footer() {
     const { nav, getChapter, getBook } = useContext(MainContext)
     function pageTurn(e) {
-        const {value} = e.target
-        let num = Math.abs(value + nav.chapter)
+        const { value } = e.target
+        let num = Math.abs(Number(value) + Number(nav.chapter))
 
-        if (num < nav.chapters.length && num > 1) {
+        if (num <= nav.chapters.length && num >= 1) {
             getChapter(num)
         }
     }
 
-    return(
+    return (
         <div className='container fixed-bottom'>
             <div className='row'>
-                
-
-                    <div className='col col-2'>
+                <div className='col col-2'>
                     <button value={-1} onClick={pageTurn} className='btn mb-4 btn-primary'>Previous</button>
                 </div>
                 <div className='col col-8'></div>
                 <div className='col col-2'>
-
-                    
                     <button value={1} onClick={pageTurn} className='btn mb-4 btn-primary'>Next</button>
                 </div>
-             
-            
             </div>
         </div>
     )
